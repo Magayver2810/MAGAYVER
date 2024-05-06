0d85c4a680c0ef33125b0d1eeba0e824316c808b
Magayver2810 <161333370+Magayver2810@users.noreply.github.com>
2024-05-06 08:33:30 -0300

    Add files via upload

--- objetivos/main.js
+++ objetivos/main.js
@@ -1,7 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document. querySelectorAll(".contador");
-const tempoObjetivo1= new Date("2024-10-05T12:30:00");
+const tempoObjetivo1= new Date("2032-12-17T21:00:00");
+const tempoObjetivo2= new Date("2034-05-12T18:00:00");
+const tempoObjetivo3= new Date("2042-07-22T13:00:00");
+const tempoObjetivo4= new Date("2033-04-18T20:00:00");
 const tempoAtual= new Date;
 //Declara quais elementos vão ser lidos. O script vai ler todos que tiverem a classe "botao".
 
@@ -23,4 +26,25 @@ for(let i=0;i <botoes.length;i++){
     }
 }
 
-contadores[0].textContent= tempoObjetivo1 - tempoAtual;
+
+
+contadores[0].textContent= calculaTempo(tempoObjetivo1);
+contadores[1].textContent= calculaTempo(tempoObjetivo2);
+contadores[2].textContent= calculaTempo(tempoObjetivo3);
+contadores[3].textContent= calculaTempo(tempoObjetivo4);
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual= new Date();
+    let tempoFinal= tempoObjetivo - tempoAtual;
+    let segundos= Math.floor(tempoFinal/1000);
+    let minutos= Math.floor(segundos/60);
+    let horas= Math.floor(minutos/60);
+    let dias= Math.floor(horas/24);
+
+    segundos %=60;
+    minutos %=60;
+    horas %=24;
+
+    return dias+" dias "+horas+" horas "+minutos+" minutos "+segundos+" segundos ";
+    
+}
