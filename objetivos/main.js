6a35aaed670d0d067c1554ad012a21990c08694d
Magayver2810 <161333370+Magayver2810@users.noreply.github.com>
2024-05-13 10:10:50 -0300

    Add files via upload

--- objetivos/main.js
+++ objetivos/main.js
@@ -23,15 +23,10 @@ for(let i=0;i <botoes.length;i++){
         textos[i].classList.add("ativo");
 
         //Adiciona a classe ativo só no botão clicado.
-    }
+}
 }
 
 
-
-contadores[0].textContent= calculaTempo(tempoObjetivo1);
-contadores[1].textContent= calculaTempo(tempoObjetivo2);
-contadores[2].textContent= calculaTempo(tempoObjetivo3);
-contadores[3].textContent= calculaTempo(tempoObjetivo4);
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual= new Date();
@@ -45,6 +40,19 @@ function calculaTempo(tempoObjetivo){
     minutos %=60;
     horas %=24;
 
-    return dias+" dias "+horas+" horas "+minutos+" minutos "+segundos+" segundos ";
-    
+    return dias+" dias "+horas+" horas "+minutos+" minutos "+segundos+" segundos ";  
+}
+
+comecaCronometro();
+
+function comecaCronometro(){
+   atualizaCronometro();
+   setInterval(atualizaCronometro,1000);
+}
+
+function atualizaCronometro(){
+    contadores[0].textContent = calculaTempo(tempoObjetivo1);
+    contadores[1].textContent = calculaTempo(tempoObjetivo2);
+    contadores[2].textContent = calculaTempo(tempoObjetivo3);
+    contadores[3].textContent = calculaTempo(tempoObjetivo4);
 }
